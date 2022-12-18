b59a4ade71d8a75ffa648476050f4386f2dcdece
sadik al jarif <78945097+jarif87@users.noreply.github.com>
2022-12-18 17:13:27 +0600

    Add files via upload

---
+++ hacker_rank_python_division.py
@@ -0,0 +1,15 @@
+# =============================================================================
+# he provided code stub reads two integers,  and , from STDIN.
+# 
+# Add logic to print two lines. The first line should contain the result of integer division,  // . The second line should contain the result of float division,  / .
+# 
+# No rounding or formatting is necessary.
+# =============================================================================
+
+x=int(input())
+y=int(input())
+print(x//y)
+print(x/y)
+
+
+
